80f96f3990b593c780221914c4fe3c687214b120
brian <brian@ADIEL>
2025-03-28 21:23:55 -0600

    Se creo un archivo el cual se muestra una Playlist sobre una canción.

---
+++ src/main/java/com/edu/netbeans/playlist/Playlist.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edu.netbeans.playlist;
+
+/**
+ *
+ * @author brian
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas p1 = new Musicas("El paso del gigante","Cumbias Mexicanas","1987",3);
+        p1.ShowPlaylist();
+    }
+}
